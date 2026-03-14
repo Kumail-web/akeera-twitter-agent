1d7c63c4775a1a54881e9956e5ae14fe63258ada
Kumail-web <64539838+Kumail-web@users.noreply.github.com>
2026-03-14 13:09:24 +0530

    Create token.js

---
+++ api/token.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.status(200).end();
+
+  const { code, code_verifier, client_id, redirect_uri } = req.body;
+
+  const response = await fetch("https://api.twitter.com/2/oauth2/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      code,
+      grant_type: "authorization_code",
+      client_id,
+      redirect_uri,
+      code_verifier,
+    }).toString(),
+  });
+
+  const data = await response.json();
+  return res.status(200).json(data);
+}
